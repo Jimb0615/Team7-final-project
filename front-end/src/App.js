6d9a665ebef29f20458db7c9709e6e38d86220b0
Kate Filmer <saravana@sara.com>
2025-07-22 16:04:44 -0400

    style & color changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundImage: 'radial-gradient(#ccc 1px, transparent 1px)',
-        backgroundSize: '20px 20px',
-        backgroundColor: '#ffffff',
-        minHeight: '100vh',
-      }}
-    >
-      <header className="app-header">
-        <h1>Git'r Done News Reader</h1>
-        <p>Shift gears to smarter news — save it, read it, Git’r Done!</p>
-      </header>
-      <NewsReader />
-    </div>
+  <div className="dotted-bg">
+  <header className="app-header">
+    <h1>Git'r Done News Reader</h1>
+    <p>Shift gears to smarter news — save it, read it, Git’r Done!</p>
+  </header>
+  <NewsReader />
+</div>
   );
 }
 
